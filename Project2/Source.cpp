5614ca2d224122857f9736af66fe512788656ed3
ADMIN <ADMIN@DESKTOP-EVD1RCJ>
2022-08-14 20:27:13 +0700

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int max = 0;
+    cout << "\n Nhập 2 số nguyên a và b: ";
+    cin >> a; cin >> b;
+    if (a > b)
+    {
+        max = a;
+        cout << max;
+    }
+    else
+    {
+        max = b;
+        cout << max;
+    }
+}
